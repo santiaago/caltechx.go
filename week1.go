2d7759db0669d4a8b1a4a66ccd235848ec1fdbcf
santiaago <santiago.ariassar@gmail.com>
2014-10-23 13:33:13 +0100

    pla 1st shot

--- week1.go
+++ week1.go
@@ -6,11 +6,11 @@ import (
 	"time"
 )
 
-type point struct {
-	x0 float64 // artificial coordinate for the perceptron learning algorithm
-	x  float64
-	y  float64
-}
+// type point struct {
+// 	x0 float64 // artificial coordinate for the perceptron learning algorithm
+// 	x  float64
+// 	y  float64
+// }
 
 type linear_function struct {
 	a float64
@@ -42,15 +42,49 @@ func (l *linear_function) print() {
 	fmt.Printf("func: %vX + %v\n", l.a, l.b)
 }
 
-func (p *point) print() {
-	fmt.Printf("point: x:%v \ty:%v\n", p.x, p.y)
+type point [3]float64
+
+func (pt *point) print() {
+	p := *pt
+	fmt.Printf("point: x0:%v \tx1:%v \tx2:%v\n", p[0], p[1], p[2])
 }
 
 func evaluate(f linear_func, p point) int {
-	if p.y < f(p.x) {
+	if p[2] < f(p[1]) {
 		return -1
 	}
 	return 1
+}
+
+func sign(p float64) int {
+	if p > float64(0) {
+		return 1
+	}
+	return -1
+}
+
+// perceptron implements h(x) = sign(w'x)
+func h(x point, w point) int {
+	if len(x) != len(w) {
+		fmt.Println("Panic: vectors x and w should be of same size.")
+		panic(x)
+	}
+	var res float64 = 0
+	for i := 0; i < len(w); i++ {
+		res = res + w[i]*x[i]
+	}
+	return sign(res)
+}
+
+// build misclassified set.
+func build_misclassified_set(Xn []point, Yn []int, Wn []point) []int {
+	var set []int
+	for i := 0; i < len(Wn); i++ {
+		if h(Xn[i], Wn[i]) != Yn[i] {
+			set = append(set, i) //set.Append(i)
+		}
+	}
+	return set
 }
 
 func main() {
@@ -65,12 +99,15 @@ func main() {
 	fmt.Println(randInInterval(-1, 1))
 	f := randLine()
 	f.print()
-	var Xn [10]point
-	var Yn [10]int
-	var Wn [10]point
+	//var Xn [10]point //[3]float64
+	Xn := make([]point, 10)
+	Yn := make([]int, 10)
+	Wn := make([]point, 10)
 	for i := 0; i < 10; i++ {
-		Wn[i] = point{0, 0, 0}
-		Xn[i] = point{1, randInInterval(-1, 1), randInInterval(-1, 1)}
+		//Wn[i] = point{0, 0, 0} no need to do this as it is automatically set to zero.
+		Xn[i][0] = float64(1)
+		Xn[i][1] = randInInterval(-1, 1)
+		Xn[i][2] = randInInterval(-1, 1)
 		Xn[i].print()
 		Yn[i] = evaluate(f.getFunc(), Xn[i])
 	}
@@ -79,4 +116,30 @@ func main() {
 		fmt.Printf("Y%v : %v\n", i, Yn[i])
 	}
 
+	n_iterations := 0
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for {
+		fmt.Printf("iteration #%v\n", n_iterations)
+		// pick a misclasified number
+		// first build a misclassified set:
+		misclasified_set := build_misclassified_set(Xn, Yn, Wn)
+		fmt.Println("number of misclassified points: ", len(misclasified_set))
+		for i := 0; i < len(Wn); i++ {
+			fmt.Printf("W0: %v \t W1: %v \t W2: %v\n", Wn[0], Wn[1], Wn[2])
+		}
+		if len(misclasified_set) == 0 {
+			break
+		}
+		rand_point := r.Intn(len(misclasified_set))
+		// update the weight vector:
+		// w <-- w + YnXn
+		Wn[rand_point][0] = Wn[rand_point][0] + float64(Yn[rand_point])*Xn[rand_point][0]
+		Wn[rand_point][1] = Wn[rand_point][1] + float64(Yn[rand_point])*Xn[rand_point][1]
+		Wn[rand_point][2] = Wn[rand_point][2] + float64(Yn[rand_point])*Xn[rand_point][2]
+
+		n_iterations += 1
+		if n_iterations > 10 {
+			break
+		}
+	}
 }
