d4520f14a5dea0345baaf6e7318a7fe29b9fb214
santiaago <santiago.ariassar@gmail.com>
2014-10-26 21:10:29 +0000

    normal hoeffding inequality

--- week2.go
+++ week2.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
-	//"sync"
+	//	"sync"
 	"time"
 )
 
@@ -18,54 +18,56 @@ func sum(a []int) int {
 	}
 	return r
 }
+
+type experiment struct {
+	NRuns    int
+	NCoins   int
+	NFlips   int
+	AvgV1    float64
+	AvgVRand float64
+	AvgVMin  float64
+}
+
 func q1() {
 	start := time.Now()
-	nRuns := 100000
-	nCoins := 1000
-	nFlips := 10
-	avgV1 := float64(0)
-	avgVRand := float64(0)
-	avgVMin := float64(0)
-	//c1
+	e := experiment{NRuns: 100000, NCoins: 1000, NFlips: 10}
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	//var wg sync.WaitGroup
-	for run := 0; run < nRuns; run++ {
+	var sumV1, sumVRand, sumVMin float64
+	for run := 0; run < e.NRuns; run++ {
 		//wg.Add(1)
 		//go func() {
-		coins := make([][]int, nCoins)
+		coins := make([][]int, e.NCoins)
 		for i, _ := range coins {
-			coins[i] = make([]int, nFlips)
+			coins[i] = make([]int, e.NFlips)
 			for j := 0; j < len(coins[i]); j++ {
 				coins[i][j] = r.Intn(2)
 			}
 		}
-		v1 := float64(float64(sum(coins[0])) / float64(nFlips))
-		randIndex := r.Intn(nCoins - 1)
-		vRand := float64(float64(sum(coins[randIndex])) / float64(nFlips))
+		v1 := float64(float64(sum(coins[0])) / float64(e.NFlips))
+		randIndex := r.Intn(e.NCoins - 1)
+		vRand := float64(float64(sum(coins[randIndex])) / float64(e.NFlips))
 		vMin := math.Inf(1)
 		for i, _ := range coins {
-			vCurrent := float64(float64(sum(coins[i])) / float64(nFlips))
+			vCurrent := float64(float64(sum(coins[i])) / float64(e.NFlips))
 			if vCurrent < vMin {
 				vMin = vCurrent
 			}
 		}
-		avgV1 += v1
-		avgVRand += vRand
-		avgVMin += vMin
-		//c <- 1
-		//	wg.Done()
+		sumV1 += v1
+		sumVRand += vRand
+		sumVMin += vMin
+		//wg.Done()
 		//}()
 	}
-	// go func() {
-	// 	wg.Wait()
-	avgV1 = avgV1 / float64(nRuns)
-	avgVRand = avgVRand / float64(nRuns)
-	avgVMin = avgVMin / float64(nRuns)
-	//	}()
-	fmt.Printf("V1: %4.2f\nVRand: %4.2f\nVMin: %4.2f\n", avgV1, avgVRand, avgVMin)
+	//wg.Wait()
+	e.AvgV1 = sumV1 / float64(e.NRuns)
+	e.AvgVRand = sumVRand / float64(e.NRuns)
+	e.AvgVMin = sumVMin / float64(e.NRuns)
+	fmt.Printf("V1: %4.2f\nVRand: %4.2f\nVMin: %4.2f\n", e.AvgV1, e.AvgVRand, e.AvgVMin)
 
 	elapsed := time.Since(start)
-	fmt.Printf("q1 took %s", elapsed)
+	fmt.Printf("q1 took %s\n", elapsed)
 }
 
 func main() {
