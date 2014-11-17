0ebf8b366f9d70f06875ddb42da09d95c5254041
santiaago <santiago.ariassar@gmail.com>
2014-11-17 22:09:55 +0000

    week 5 initial commit

---
+++ week5.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+// measure will measure the time taken by function f to run and display it.
+func measure(f func(), name string) {
+	start := time.Now()
+	f()
+	elapsed := time.Since(start)
+	fmt.Printf("%s took %4.2f seconds\n", name, elapsed.Seconds())
+}
+
+func main() {
+	fmt.Println("Num CPU: ", runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	fmt.Println("week 5")
+	//measure(q1, "q1")
+	fmt.Println("1")
+	fmt.Println("2")
+	fmt.Println("3")
+	fmt.Println("4")
+	fmt.Println("5")
+	fmt.Println("6")
+	fmt.Println("7")
+	fmt.Println("8")
+	fmt.Println("9")
+	fmt.Println("10")
+}
